f45d2be18708df58036f86910a960b8d51b6188c
SantiagoGMG <al064489@uacam.mx>
2024-11-05 19:34:42 -0600

    feat: :hammer: Se añadió funcionalidades al juego

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿//using Android.Print;
+using System;
 using System.ComponentModel;
+using System.Diagnostics;
 
 namespace Hangman
 {
@@ -50,14 +52,52 @@ namespace Hangman
             }
         }
 
+        //private string mensaje = "";
+
+        public string Mensaje
+        {
+            get => lblPerdiste.Text;
+            set
+            {
+                lblPerdiste.Text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Estado
+        {
+            get => lblErrores.Text;
+            set
+            {
+                lblErrores.Text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int errores;
+        private ImageSource Imagen
+        {
+            get => imgHangman.Source;
+            set
+            {
+                imgHangman.Source = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        private string adivinar;
+
         public MainPage()
         {
             InitializeComponent();
             Letras.AddRange("qwertyuiopasdfghjklzxcvbnm".ToCharArray());
             BindingContext = this;
 
-            string adivinar = ElegirPalabra();
+            adivinar = ElegirPalabra();
             EnmascararPalabra(adivinar, ListaLetrasUsuario);
+            lblPerdiste.IsVisible = false;
         }
 
         private string ElegirPalabra()
@@ -75,7 +115,79 @@ namespace Hangman
             
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            //se obtiene el strign del boton pero es necesario convertirlo a char, y usamos el método toCharArray y se agarra la posición 0
+            char letraBoton = boton.Text.ToCharArray()[0];
+            boton.IsEnabled = false;
+            //imgHangman.Source = "img2.jpg";
+            //Debug.WriteLine($"Letra presionada: {letraBoton}");
+            ManejadorLetra(letraBoton);
+            
+        }
 
+        private void ManejadorLetra(char letraClickeada)
+        {
+            if(ListaLetrasUsuario.IndexOf(letraClickeada) == -1)
+            {
+                ListaLetrasUsuario.Add(letraClickeada);
+            }
+            if(adivinar.IndexOf(letraClickeada) >=0)
+            {
+                EnmascararPalabra(adivinar, ListaLetrasUsuario);
+                RevisarSiGano();
+            }
+            else
+            {
+                errores = errores + 1;
+                ActualizarEstado();
+            }
+        }
+
+        private void RevisarSiGano()
+        {
+            if(Spotlight.Replace(" ", "") == adivinar)
+            {
+                Mensaje = "Ganador!";
+                lblPerdiste.IsVisible = true;
+            }
+        }
+
+        private void ActualizarEstado()
+        { 
+
+            Estado = $"Errores: {errores} de 6";
+            Imagen = $"img{errores}.jpg";
+            if(errores == 6)
+            {
+                Mensaje = "Perdiste!";
+                lblPerdiste.IsVisible = true;
+                Spotlight = adivinar;
+            }
+        }
+
+        private void btnReiniciar_Clicked(object sender, EventArgs e)
+        {
+            Mensaje = "Perdiste!";
+            lblPerdiste.IsVisible = false;
+            lblErrores.Text = "Errores: 0 de 6";
+            errores = 0;
+            imgHangman.Source = "img0.jpg";
+            Spotlight = "";
+            ListaLetrasUsuario.Clear();
+            adivinar = ElegirPalabra();
+            EnmascararPalabra(adivinar, ListaLetrasUsuario);
+
+            //sirve para reactivar los botones
+            foreach (var view in contenedorBotones.Children.OfType<Button>())
+            {
+                if (view is Button button)
+                {
+                    button.IsEnabled = true;
+                }
+            }
+        }
     }
 
 }
